66fb0253fb9da5dd94450d3382af6645a7bd3b85
Roger Aspelin <roger.aspelin@hotmail.se>
2020-09-18 00:55:24 +0200

    Connection to Timer

--- TCPTest/Program.cs
+++ TCPTest/Program.cs
@@ -6,7 +6,42 @@ namespace TCPTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string address = "127.0.0.1";
+            int port = 2312;
+
+
+            var client = new TimerClient(address, port);
+            Console.Write("Client connecting...");
+            client.ConnectAsync();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the client or '!' to reconnect the client...");
+
+            // Perform text input
+            for (; ; )
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Disconnect the client
+                if (line == "!")
+                {
+                    Console.Write("Client disconnecting...");
+                    client.DisconnectAsync();
+                    Console.WriteLine("Done!");
+                    continue;
+                }
+
+                // Send the entered text to the chat server
+                client.SendAsync(line);
+            }
+
+            // Disconnect the client
+            Console.Write("Client disconnecting...");
+            client.DisconnectAndStop();
+            Console.WriteLine("Done!");
+
         }
     }
 }
---
+++ TCPTest/TimerClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using TcpClient = NetCoreServer.TcpClient;
+
+namespace TCPTest
+{
+    class TimerClient : TcpClient
+    {
+        private bool _stop;
+        public TimerClient(string address, int port): base(address, port)
+        {
+
+        }
+
+        public void DisconnectAndStop()
+        {
+            _stop = true;
+            DisconnectAsync();
+            while (IsConnected)
+                Thread.Yield();
+        }
+
+        protected override void OnConnected()
+        {
+            Console.WriteLine($"Connected to timer with session id {Id}");
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(buffer, (int)offset, (int)size));
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP client caught an error with code {error}");
+        }
+    }
+}
