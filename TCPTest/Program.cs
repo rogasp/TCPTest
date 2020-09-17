b59ebba5c9c749186576b679af6b530431519e43
Roger Aspelin <roger.aspelin@hotmail.se>
2020-09-18 00:27:52 +0200

    Add project files.

---
+++ TCPTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
